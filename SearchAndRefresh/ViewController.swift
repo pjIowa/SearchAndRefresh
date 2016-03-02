5a7e670686a43c8b6503abb93d17fa5d06a60ed3
pjIowa <pjkedilaya@gmail.com>
2016-03-01 20:14:57 -0700

    Initial Commit

---
+++ SearchAndRefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchAndRefresh
+//
+//  Created by Prajwal Kedilaya on 3/1/16.
+//  Copyright © 2016 Prajwal Kedilaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
